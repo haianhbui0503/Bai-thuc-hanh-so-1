05872c7c7283049d9f69e4ce89bbc948e06e1a25
haianhbui0503 <haianhhbui0503@gmail.com>
2026-09-21 21:31:21 +0700

    Update buoi1.py

--- buoi1.py
+++ buoi1.py
@@ -1,5 +1,34 @@
 #Bai 1
-name= Hai Anh
-age=18
-school=PTIT
+name= "Hai Anh"
+age = 18
+school = "PTIT"
 print(f"Xin chào, tôi là {name}, năm nay {age} tuổi, đang học tại {school}")
+#Bài2
+chieu_dai = float(input())
+chieu_rong = float(input())
+chu_vi = (chieu_dai + chieu_rong) * 2
+dien_tich= chieu_dai * chieu_rong
+#Bai3
+C = int(input("Nhập số: "))
+F = C * 9/5 + 32
+print(C, "độ C bằng", F, "độ F")
+#Bai4
+Toan = float(input("Điểm Toán: "))
+Van = float(input("Điểm Văn: "))
+Anh = float(input("Điểm Anh: "))
+trung_binh= (Toan + Van + Anh) /3
+print(round(trung_binh, 2))
+#Bai5
+ten = "Hai Anh"
+tuoi = 18
+print(f"Tôi tên là {ten}, tôi {tuoi} tuổi")
+print(f"Sang năm tôi sẽ {tuoi + 1} tuổi")
+x= int(input("Nhập số: "))
+if x % 2 ==0:
+  print ("Số chẵn")
+else:
+  print("Số lẻ")
+
+
+
+
